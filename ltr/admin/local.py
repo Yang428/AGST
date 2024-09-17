a4bbd6cba26a44a7994c73388edff7c880024a62
Yang <75066164+Yang428@users.noreply.github.com>
2024-09-17 22:10:29 +0800

    Update local.py

--- ltr/admin/local.py
+++ ltr/admin/local.py
@@ -2,12 +2,12 @@ class EnvironmentSettings:
     def __init__(self):
         self.workspace_dir = '/home/wcz/Yang/AGST/ltr/checkpoints/'    # Base directory for saving network checkpoints.
         self.tensorboard_dir = self.workspace_dir + '/tensorboard/'    # Directory for tensorboard files.
-        self.lasot_dir = '/media/wcz/disk2/transtrack/TransT_train/dataset/LaSOT/LaSOTBenchmark/'
-        self.got10k_dir ='/media/wcz/disk2/transtrack/TransT_train/dataset/Got10k/train/'
+        self.lasot_dir = '/media/wcz/disk2/dataset/LaSOT/LaSOTBenchmark/'
+        self.got10k_dir ='/media/wcz/disk2/dataset/Got10k/train/'
         self.trackingnet_dir = ''
         self.coco_dir = ''
         self.imagenet_dir = ''
         self.imagenetdet_dir = ''
-        self.pregenerated_masks = '/media/wcz/disk2/transtrack/TransT_train/dataset/pre_masks/'
-        self.davis_dir = '/media/wcz/disk2/transtrack/TransT_train/dataset/DAVIS/'
-        self.vos_dir = '/home/wcz/Yang/JCAT_train/ltr/dataset/YouTube_VOS/train/'
+        self.pregenerated_masks = '/media/wcz/disk2/dataset/pre_masks/'
+        self.davis_dir = '/media/wcz/disk2/dataset/DAVIS/'
+        self.vos_dir = '/home/wcz/Yang/AGST_train/ltr/dataset/YouTube_VOS/train/'
